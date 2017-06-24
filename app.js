d5fe64fe46fc4877b87aa57fec787bdc892c546c
rlyle78@gmail.com <rlyle78@gmail.com>
2017-06-24 11:53:31 +0000

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+	console.log(session.message.text);
+	session.say(session.message.text, session.message.text);
+
+	//var msg = new builder.Message(session).speak('<speak>' + session.message.text + '</speak>');
+    	//session.send("You said: %s", session.message.text);
+	//session.send(msg).endDialog();
+});
